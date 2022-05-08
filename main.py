81eaed6ede11c34c1f0a52558320affb285c5f90
Mesut Arslan <mesutarslan721@gmail.com>
2022-05-08 16:57:45 +0300

    message

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import cv2
-
-img = cv2.imread("fall001.jpg")
-i_height,i_width = img.shape[:2] #firt dimeansion
+import sys
+filename = sys.argv[1]
+annatation_filename = sys.argv[2]
+print(filename)
+img = cv2.imread(filename)
+i_height,i_width = img.shape[:2] #firt two dimeansion
 window_name = 'cerceve'
 
 # YOLO DEGERLERI (0 0.506757 0.548230 0.445946 0.241148)
 
                 #<object-class> <x> <y> <width> <height>
-#Array unpack
-center_x, center_y, width, height = [0.506757, 0.548230, 0.445946, 0.241148]
+'''Array umpack'''
+# Open a file: file
+file = open(annatation_filename, mode='r')
+
+# read all lines at once
+all_of_it = file.read()
+
+# close the file
+file.close()
+list_of_elements = all_of_it.split(" ")
+"accessing elements list in  for loops"
+center_x, center_y, width, height = list_of_elements[1:] #[0.506757, 0.548230, 0.445946, 0.241148]
+mylist = [center_x, center_y, width, height]
+mylist = [float(i) for i in mylist]
+center_x, center_y, width, height = mylist
+
 print(center_x)
-# YOLO DAN BBOX FORMATINA GITME YOLU
+print(center_y)
+print(width)
+print(height)
+print(i_width)
+'''center_x = float(center_x)
+center_y = float(center_y)
+width = float(width)
+height = float(height)
+# YOLO DAN BBOX FORMATINA GITME YOLU'''
 min_x = int(i_width * (center_x - (width/2)))
 min_y = int(i_height * (center_y - (height/2)))
 max_x = int(i_width * width + min_x)
